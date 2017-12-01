33ec58228efd43b04fc3312b96169df44d663b0d
strelok <strelok@stappler.org>
2017-12-01 18:25:00 +0800

    added printing function

--- wasm/c/hello.c
+++ wasm/c/hello.c
@@ -1,8 +1,18 @@
 #include "hello.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 extern void import_function(int);
+extern void print_function(char *);
 
 int export_function(int i_test) {
+  print_function("Testing print import");
   import_function(header_function(i_test));
   return ++i_test;
 }
+
+char *print_export(){
+	return "Print export text";
+}
+
